bffb57ef77f10a3f9c0bdd1a52b55e9495c2072e
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-09-15 15:14:24 -0400

    remove starship docker indicator

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,2 @@
-if vim.g.neovide then
-  vim.o.guifont = 'JetBrainsMono Nerd Font'
-
-  vim.g.neovide_floating_blur_amount_x = 2.0
-  vim.g.neovide_floating_blur_amount_y = 2.0
-
-  vim.g.neovide_refresh_rate = 144
-  vim.g.neovide_refresh_rate_idle = 5
-end
 require 'talonvim.core'
 vim.cmd [[colorscheme tokyonight]]
